faf8b7e748651585c4c5a90c862f66d7aeface30
katharina.starzer@eodc.eu <katharina.starzer@eodc.eu>
2025-05-14 21:10:10 +0200

    new links

--- conf.py
+++ conf.py
@@ -4,34 +4,28 @@ import os
 import sys
 import pathlib
 
-# -- Path setup ----------------------------------------------------------------
 
-# Make _extensions importable
 sys.path.insert(0, os.path.abspath("_extensions"))
 
-# -- Conditional Extension Loading --------------------------------------------
 
-# Load notebook_gallery_generator only when building the central docs/ page
-if pathlib.Path(__file__).parent.resolve().name == "docs":
-    extensions = [
-        "sphinx.ext.githubpages",
-        "myst_nb",
-        "sphinx_design",
-        "notebook_gallery_generator",
-    ]
-else:
-    extensions = [
-        "sphinx.ext.githubpages",
-        "myst_nb",
-        "sphinx_design",
-    ]
+extensions = [
+    "sphinx.ext.githubpages",  # Generiert .nojekyll für GitHub Pages
+    "myst_nb",                 # Für Markdown + Jupyter Notebooks (Myst-NB)
+    "sphinx_design",           # Für schöne UI-Elemente (Tabs, Buttons, etc.)
+]
 
-# -- Project information -------------------------------------------------------
+# Aktiviere diese Erweiterung nur, wenn die Umgebungsvariable GALLERY_BUILD gesetzt ist.
+# → Das wird im zentralen Build-Workflow gemacht:
+#    GALLERY_BUILD=1 sphinx-build -b html . _temp_gallery
+if "GALLERY_BUILD" in os.environ:
+    extensions.append("notebook_gallery_generator")
+
+
 project = 'Jupyter Notebook Gallery'
 copyright = '2024, Destination Earth Data Lake'
 author = 'Christoph Reimer <christoph.reimer@eodc.eu>'
 
-# -- General configuration -----------------------------------------------------
+
 myst_enable_extensions = [
     "amsmath",
     "colon_fence",
@@ -40,27 +34,45 @@ myst_enable_extensions = [
     "dollarmath",
 ]
 
+# Erlaubte Links und Anker in Überschriften
 myst_url_schemes = ["http", "https", "mailto"]
 myst_heading_anchors = 3
+
+# Notebooks NICHT ausführen beim Bauen
 nb_execution_mode = 'off'
 
+# Quelle: Markdown und Notebooks
 source_suffix = {
     ".ipynb": "myst-nb",
+    ".md": "markdown",
     ".myst": "myst-nb",
 }
 
+# Verzeichnisse für Templates
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', "**/.git", ".pixi**"]
 
-# -- HTML output configuration -------------------------------------------------
+# Dateien/Ordner, die ignoriert werden
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    '**/.git',
+    '.pixi*',
+]
+
+
 html_theme = 'pydata_sphinx_theme'
+
+# Optional: eigene statische Ressourcen (CSS, Bilder)
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 
+# Logos, Titel, Icons
 html_logo = "https://hda.data.destination-earth.eu/ui/images/destination_earth_logo_W.svg"
 html_favicon = "https://hda.data.destination-earth.eu/ui/images/favicon.svg"
 html_title = "DEDL Notebook Gallery"
 
+# Theme-Optionen
 html_theme_options = {
     "logo": {
         "image_light": html_logo,
@@ -82,6 +94,5 @@ html_theme_options = {
     ],
 }
 
-# -- Entry point for Sphinx ----------------------------------------------------
-master_doc = 'index'
 
+master_doc = 'index'
